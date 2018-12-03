9b4cf56bc28d0ca79caac6c6e99e918f15c66689
Shawanda Davis <shawandadavis93@gmail.com>
2018-12-03 21:47:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+  if num <= 1 
+    return FALSE
+  else
+    !(2...num).to_a.any? do |int|
+      num % int == 0  
+    end
+  end  
+  
+end   
