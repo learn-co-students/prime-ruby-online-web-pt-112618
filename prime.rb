e2ff2c646ba318e4da5428dbab64591bef6553d0
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 09:30:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  t=true
+  if (number<=1)
+    t=false
+    return t
+  end
+   if (number==2)
+    t=true
+    return t
+  end
+  for i in (2 .. number-1)
+  if number%i==0 
+    t=false
+     break
+  end
+  i+=1
+end
+ return t
+end
